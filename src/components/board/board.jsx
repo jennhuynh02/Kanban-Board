bb08514ff71dc19b66ecc2ebc2e2887a718ce559
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-29 17:05:13 -0700

    Add editing feature for column names

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -1,15 +1,38 @@
 import React from "react";
+import './board.css';
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      columns: ["Todo", "In progress", "Done"],
+    }
+
+    this.update = this.update.bind(this);
+  }
+
+  update(idx) {
+    return (e) => {
+      let copy = this.state.columns;
+      copy[idx] = e.currentTarget.value;
+      this.setState({ columns: copy });
+    }
+  }
+
+  swap() {
+    
   }
 
   render() {
+    console.log(this.state);
     return (
-        <div>
-            
-        </div>
+      <div className="all-columns">
+        {this.state.columns.map((col, idx) => (
+          <div key={idx} draggable="true" className="column">
+            <input defaultValue={col} onChange={this.update(idx)}/>
+          </div>
+        ))}
+      </div>
     )
   }
 }
