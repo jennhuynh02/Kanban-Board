2dc2bef659379564998b7b284f109a8ffb8b39ba
Michael Murry <michaelmurry49@gmail.com>
2020-08-29 17:23:57 -0700

    update card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,49 @@
 import React from "react";
-import CardContainer from "./column_container";
 
 class Card extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            title: props.title,
+            description: props.description,
+            hidden: true,
+        }
+    }
+
+    updateTitle(t){
+        this.setState({title: t});
+    }
+
+    updateDescription(desc){
+        this.setState({desc: desc});
+    }
+
+    deleteCard(){
+        // delete code here
+        // need to interact with local storage?
+    }
+
+    toggleCard(){
+        this.setState({hidden: !this.state.hidden})
+    }
+
+    render(){
+        return (
+            <div class="card">
+                <div class="cardTitle">
+                    {this.state.title}
+                </div>
+                {this.state.hidden ?
+                    <div class="cardDescription" hidden>
+                        {this.state.description}
+                    </div> :
+                    <div class="cardDescription">
+                        {this.state.description}
+                    </div>
+                }
+                
+            </div>
+        )
     }
 }
 
