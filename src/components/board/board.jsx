a22352e5bbd662a2a9e0b25bfb93428158b3af7f
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-29 18:14:53 -0700

    Add column drag feature

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -6,9 +6,13 @@ class Board extends React.Component {
     super(props);
     this.state = {
       columns: ["Todo", "In progress", "Done"],
+      drag: null,
+      set: null,
     }
 
     this.update = this.update.bind(this);
+    this.move = this.move.bind(this);
+    this.set = this.set.bind(this);
   }
 
   update(idx) {
@@ -19,17 +23,38 @@ class Board extends React.Component {
     }
   }
 
-  swap() {
-    
+  move(idx) {
+    return (e) => {
+      e.preventDefault();
+      this.setState({ drag: idx });
+    }
+  }
+
+  set(idx) {
+    return (e) => {
+        e.preventDefault();
+      if (this.state.drag < idx) {
+        let copy = this.state.columns;
+        let a = copy[this.state.drag];
+        for (let i = this.state.drag; i <= idx; i++) {
+          copy[i] = copy[i+1];
+        }
+        copy[idx] = a;
+        this.setState({ 
+          set: idx,
+          columns: copy });
+        console.log("copy", copy)
+      }
+    }
   }
 
   render() {
-    console.log(this.state);
+    console.log(this.state)
     return (
       <div className="all-columns">
         {this.state.columns.map((col, idx) => (
-          <div key={idx} draggable="true" className="column">
-            <input defaultValue={col} onChange={this.update(idx)}/>
+          <div key={idx} draggable="true" className="column" onDrag={this.move(idx)} onDragEnter={this.set(idx)}>
+            <input value={col} onChange={this.update(idx)}/>
           </div>
         ))}
       </div>
