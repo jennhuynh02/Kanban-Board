4ec45b0b2de37f94c49f6b7ce69d7ea7d9c71ff7
Jennifer Huynh <jennhuynh02@gmail.com>
2020-08-30 20:39:04 -0700

    Updates

---
+++ precache-manifest.826b966afa590e4b75ebc89e0a5e8a74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0730d4cf5abb3a19daf2ef7bb2b5e5fb",
+    "url": "/Kanban-Board/index.html"
+  },
+  {
+    "revision": "dfe40f7d4d1577a81974",
+    "url": "/Kanban-Board/static/css/main.2bea0cd7.chunk.css"
+  },
+  {
+    "revision": "4f68b5081d9ff249bb26",
+    "url": "/Kanban-Board/static/js/2.a72be10c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kanban-Board/static/js/2.a72be10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfe40f7d4d1577a81974",
+    "url": "/Kanban-Board/static/js/main.c45d5937.chunk.js"
+  },
+  {
+    "revision": "24c1a8b79b9f01fb5f3b",
+    "url": "/Kanban-Board/static/js/runtime-main.6514b48e.js"
+  }
+]);
